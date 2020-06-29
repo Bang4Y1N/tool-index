6c4c4dcfad210305692160b8c7ba365916bf5548
Kang Deface <60571362+Bang4Y1N@users.noreply.github.com>
2020-06-29 08:49:20 +0800

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,55 @@
+<html><head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+<head>
+<link rel="shortcut icon" href="https://c.top4top.io/p_1600qngpv1.png" type="image/x-icon">
+<link rel="icon" href="https://c.top4top.io/p_1600qngpv1.png" type="image/x-icon">
+<meta charset="UTF-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:title" content="./Bang4Y1N">
+<meta name="description" content="Your site has been hacked , its because your system security was too low">
+<meta property="og:description" content="Thank you for all member of Sabang Merauke Cyber 45">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="./Bang4Y1N">
+<meta property="og:type" content="website">
+<meta name="copyright"content="Hacking">
+<meta name="theme-color" content="#000">
+<meta name="keywords" content="BIG FAMILY, BANG4Y1N , SMC 45, Defaced, Hacked, Breached, Fuckers, Shit Security,"/>
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+        <meta content='404 Error' property='og:title'/>
+        <meta content='article' property='og:type'/>
+<body oncontextmenu="return false" onkeydown="return false" onmousedown="return false">
+<title>404 Not Found</title>
+<script language="javascript"src="http://ykubnay.yn.lt/Js/salju/teal.js"type="text/javascript">
+<span style="color: #FFFF00">YELLOW SNOW</span>
+<br />
+<textarea cols="8"rows="2">
+<script language="javascript"src="http://ykubnay.yn.lt/Js/salju/yellow.js"></script>
+<style type='text/css'>body, a:hover {cursor: url(http://cur.cursors-4u.net/cursors/cur-2/cur118.cur), progress;}</style><a href='http://www.cursors-4u.com/cursor/2008/12/17/cool-grey-outer-glow-pointer.html' target='_blank' title='Cool Grey Outer Glow Pointer'><img alt='Cool Grey Outer Glow Pointer' border='0' src='http://cur.cursors-4u.net/cursor.png' style='position:absolute; top: 0px; right: 0px;'/></a>
+<style>
+*{
+ margin-top:2px;
+}
+
+body{
+    background-position: center;
+ background-color:#000000;
+ 
+    background-attachment: fixed;
+    background-size:5px;
+ background-image:url(http://i.imgur.com/rHHZqbog.jpg);
+ }
+
+</style></head>
+<!-- Cursor Exs .ani , Exs.jpg-->
+<!--Script Di bwah-->
+<body oncontextmenu="return false" onkeydown="return false" onmousedown="return false">
+<br><br><br><br><br><br><br><br><br><br>
+---><!--Tulisan Pertama--><center><font size="10" color="silver" face="courier new">404 Not Found</font>
+<br><br>
+<!-- Tulisan Kedua--><center><div id="foter" class="foter" style="position: center; width: 600px; height: 28px; margin: 0px; padding: 10px; font-size: 24px; text-align: center; color: rgb(255, 255, 255); font-family: &quot;trebuchet ms&quot;, Courier new, courier new, sans-serif; transform: transform-origin: 50% 0px 0px; background-color: rgb(0, 0, 0); border: 1px solid rgb(170, 170, 170); opacity: 0.5; ">
+<font face="courier new"><marquee color="lime" behavior="Flip" scrollamount="6" width="100%" style="width: 100%;">link yang lu tuju gada bro / filenya rusak</marquee></font></div></span><!--Tengah--><br><br><br>
+
+</center
+<!--Copyright_@lugi---></font></body></html>
---
+++ index2.php
@@ -0,0 +1,178 @@
+<html>
+<head>
+<title>[+] Tool Online [+]</title>
+<link rel="shortcut icon" href="logo.png" type="image/x-icon">
+<link rel="icon" href="logo.png" type="image/x-icon">
+</head>
+<body oncontextmenu="return false" onkeydown="return false" onmousedown="return false">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta name="description" content="Kumpulan tool online yang berguna untuk hacking , dijamin mantul :v">
+<meta name="theme-color" content="#000">
+<center>
+<style>
+body {
+ font-family: ubuntu;
+	background:url('sp.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+<style>
+html { 
+  background-image: url(image.jpg); 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+<script type="text/javascript">
+
+var snowmax=35
+var snowcolor=new Array("#AAAACC","#DDDDFF","#CCCCDD","#F3F3F3","#F0FFFF")
+var snowtype=new Array("Arial Black","Arial Narrow","Times","Comic Sans MS")
+var snowletter="*"
+var sinkspeed=0.6
+var snowmaxsize=22
+var snowminsize=8
+var snowingzone=1
+
+// Do not edit below this line
+var snow=new Array()
+var marginbottom
+var marginright
+var timer
+var i_snow=0
+var x_mv=new Array();
+var crds=new Array();
+var lftrght=new Array();
+var browserinfos=navigator.userAgent 
+var ie5=document.all&&document.getElementById&&!browserinfos.match(/Opera/)
+var ns6=document.getElementById&&!document.all
+var opera=browserinfos.match(/Opera/)  
+var browserok=ie5||ns6||opera
+
+function randommaker(range) {		
+	rand=Math.floor(range*Math.random())
+    return rand
+}
+
+function initsnow() {
+	if (ie5 || opera) {
+		marginbottom = document.body.clientHeight
+		marginright = document.body.clientWidth
+	}
+	else if (ns6) {
+		marginbottom = window.innerHeight
+		marginright = window.innerWidth
+	}
+	var snowsizerange=snowmaxsize-snowminsize
+	for (i=0;i<=snowmax;i++) {
+		crds[i] = 0;                      
+    	lftrght[i] = Math.random()*15;         
+    	x_mv[i] = 0.03 + Math.random()/10;
+		snow[i]=document.getElementById("s"+i)
+		snow[i].style.fontFamily=snowtype[randommaker(snowtype.length)]
+		snow[i].size=randommaker(snowsizerange)+snowminsize
+		snow[i].style.fontSize=snow[i].size
+		snow[i].style.color=snowcolor[randommaker(snowcolor.length)]
+		snow[i].sink=sinkspeed*snow[i].size/5
+		if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}
+		if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}
+		if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}
+		if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}
+		snow[i].posy=randommaker(2*marginbottom-marginbottom-2*snow[i].size)
+		snow[i].style.left=snow[i].posx
+		snow[i].style.top=snow[i].posy
+	}
+	movesnow()
+}
+
+function movesnow() {
+	for (i=0;i<=snowmax;i++) {
+		crds[i] += x_mv[i];
+		snow[i].posy+=snow[i].sink
+		snow[i].style.left=snow[i].posx+lftrght[i]*Math.sin(crds[i]);
+		snow[i].style.top=snow[i].posy
+		
+		if (snow[i].posy>=marginbottom-2*snow[i].size || parseInt(snow[i].style.left)>(marginright-3*lftrght[i])){
+			if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}
+			if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}
+			if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}
+			if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}
+			snow[i].posy=0
+		}
+	}
+	var timer=setTimeout("movesnow()",50)
+}
+
+for (i=0;i<=snowmax;i++) {
+	document.write("<span id='s"+i+"' style='position:absolute;top:-"+snowmaxsize+"'>"+snowletter+"</span>")
+}
+if (browserok) {
+	window.onload=initsnow
+}
+</script>
+<style type='text/css'>
+@import url(https://fonts.googleapis.com/css?family=Abel);
+@import url(http://fonts.googleapis.com/css?family=Iceberg);
+
+*, body, html{cursor: url(http://cur.cursors-4u.net/cursors/cur-9/cur863.ani), url(http://cur.cursors-4u.net/cursors/cur-9/cur863.png), default !important;}
+html {background: #111111;color: #ffffff;font-family: 'Iceberg';font-size: 13px;width: 100%;}
+li {display: inline;margin: 5px;padding: 5px;}
+table, th, td {border-collapse:collapse;font-family: Tahoma, Geneva, sans-serif;background: transparent;font-family: 'Iceberg';font-size: 13px;}
+.table_home, .th_home, .td_home {border: 1px solid #ffffff;}
+.table_home tr:hover {background-color: green;}
+.th_home tr:hover{background-color: #1a1a1a;}
+.wenk{color: lime;}
+th {padding: 10px;}
+a {color: #ffffff;text-decoration: none;}
+a:hover {color: gold;text-decoration: underline;text-shadow: 0pt 0pt 0.9em lime, 0pt 0pt 0.9em lime;color: #00ff00;text-decoration: none;}
+#tangina:hover{ background-color: #1a1a1a; }
+b {color: gold;}
+input[type=text], input[type=password],input[type=submit] {background: transparent;color: #ffffff;border: 1px solid #ffffff;margin: 5px auto;padding-left: 5px;
+    font-family: 'Abel';font-size: 13px;}
+textarea {border: 1px solid #ffffff;width: 100%;height: 400px;padding-left: 5px;margin: 10px auto;resize: none;background: transparent;color: #ffffff;font-family: 'Abel';font-size: 13px;}
+select {background: transparent;color: #ffffff;border: 1px solid #ffffff;margin: 5px auto;padding-left: 5px;font-family: 'Abel';font-size: 13px;}
+.but {background: transparent;color: #ffffff;border: 1px solid #ffffff;margin: 5px auto;padding-left: 5px;font-family: 'Abel';font-size: 13px;}
+li a:hover{text-shadow: 0pt 0pt 0.99em lime, 0pt 0pt 0.99em lime;color: #00ff00;text-decoration: none;}
+.th_home, .titulo {text-shadow: 0pt 0pt 0.99em lime, 0pt 0pt 0.99em lime;color: #00ff00;text-decoration: none;}
+.board {text-align: center;border: 1px solid rgba(255,180,0,0.4);border-radius: 4px;width: 500px;}
+.board p {font-size: 65px;font-family: 'Monoton', cursive;color: #fff;text-transform: uppercase;padding: 0;margin: 0;}
+
+</style><br>
+<center>
+<font size=4 face=courier>Kumpulan Tool Online by <font color=yellow>Bang4Y1N</font>
+<style>img[alt="www.000webhost.com"]{display:none;}</style><center>
+<img src=index.png height="220" width="220" opacity="50">
+<center><font color=#c6c5c4 size=2 face=courier new>[ <font color=#006400 face=courier new>bang4y1n@protonmail.com<font color=#c6c5c4> ]</font></center>
+<center><font color=#00FF00 size=2 >Github : </font><a href="https://github.com/Bang4Y1N">https://github.com/Bang4Y1N</a><br>
+<center><font color=#00FF00 size=2 >Donate : </font><a href="https://freewallet.org/id/bang4y1n/doge">https://freewallet.org/id/bang4y1n/doge</a><br>
+<center><font color=#00FF00 size=2 >Link : </font><a href="https://bit.ly/toolh4ck">https://bit.ly/toolh4ck</a><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/comment.php target=_blank>Ada Saran Tool ? <font color=yellow>Comment disini</a>
+<center><br><b><u></b></u><font size=5 face=helvetica color=#ffa500>List Tool</font><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/md5.php target=_blank><font size=4>[+] MD5 Encrypt [+]</a><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/facebook.php target=_blank><font size=4>[+] Check Valid Email FB [+]</a><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/ipweb.php target=_blank><font size=4>[+] Website IP Finder [+]</a><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/portsc.php target=_blank><font size=4>[+] Port Scanner [+]</a><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/obf.php target=_blank><font size=4>[+] Simple Obfuscator [+]</a><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/wptheme.php target=_blank><font size=4>[+] WP Theme Scanner [+]</a><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/fuzzer.php target=_blank><font size=4>[+] URL Fuzzer (Finder) [+]</a><br>
+<a href=http://vieclamtai.com/Magento_Checkout/js/tool/obfuscator.php target=_blank><font size=4>[+] PHP Auto Obfuscator [+]</a><br>
+<a href=https://contabililondra.co.uk/ghtml.php target=_blank><font size=4>[+] HTML Code Grab [+]</a><br>
+<a href=https://www.w3schools.com/html/tryit.asp?filename=tryhtml_basic target=_blank><font size=4>[+] HTML Live Editor [+]</a><br>
+<br>
+<a href=index.php><font size=3><b>BACK</b></font></a><br><br>
+<font size=1>
+<?php
+print "".$_SERVER['REMOTE_ADDR'];
+?>
+<html>
+<br>
+<?php
+print "" .$_SERVER['HTTP_USER_AGENT'];
+?>
+
